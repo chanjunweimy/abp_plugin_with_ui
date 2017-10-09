84b2d6401c70f61a5e148fb448b4dce7eea0ba33
Chan Jun Wei <chanjunweimy@gmail.com>
2017-10-09 18:24:46 +0800

    adding download file controller
    Signed-off-by: Chan Jun Wei <chanjunweimy@gmail.com>

--- aspnet-core/src/Todo.MainProject.Web.Host/Controllers/PluginController.cs
+++ aspnet-core/src/Todo.MainProject.Web.Host/Controllers/PluginController.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 using Abp.AspNetCore.Mvc.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Todo.MainProject.Controllers;
@@ -23,5 +25,52 @@ namespace Todo.MainProject.Web.Host.Controllers
         {
             return _pluginService.GetPluginObjects();
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Download(string filename)
+        {
+            if (filename == null)
+                return Content("filename not present");
+
+            var path = Path.Combine(
+                Directory.GetCurrentDirectory(),
+                "wwwroot", filename);
+
+            var memory = new MemoryStream();
+            using (var stream = new FileStream(path, FileMode.Open))
+            {
+                await stream.CopyToAsync(memory);
+            }
+            memory.Position = 0;
+            return File(memory, GetContentType(path), Path.GetFileName(path));
+        }
+
+        private string GetContentType(string path)
+        {
+            var types = GetMimeTypes();
+            var ext = Path.GetExtension(path).ToLowerInvariant();
+            return types[ext];
+        }
+
+        private Dictionary<string, string> GetMimeTypes()
+        {
+            return new Dictionary<string, string>
+            {
+                {".txt", "text/plain"},
+                {".pdf", "application/pdf"},
+                {".doc", "application/vnd.ms-word"},
+                {".docx", "application/vnd.ms-word"},
+                {".xls", "application/vnd.ms-excel"},
+                {".png", "image/png"},
+                {".jpg", "image/jpeg"},
+                {".jpeg", "image/jpeg"},
+                {".gif", "image/gif"},
+                {".csv", "text/csv"},
+                {".ico", "image/x-icon"},
+                {".js", "application/javascript"},
+                {".html", "text/html" },
+                {".css", "text/css" }
+            };
+            }
     }
 }
