8f85cc302031e0a650f9ebc4f9a6fb3915dd2bd4
Chan Jun Wei <chanjunweimy@gmail.com>
2017-10-19 17:44:24 +0800

    move AngularUI stuff to web.core to embed the original sourcecode
    Signed-off-by: Chan Jun Wei <chanjunweimy@gmail.com>

--- aspnet-core/Todo.DemoPlugin/Todo.DemoPluginDeploy.ps1
+++ aspnet-core/Todo.DemoPlugin/Todo.DemoPluginDeploy.ps1
@@ -1,8 +1,6 @@
 dotnet restore
 
 $currentPath = (Get-Item -Path "./" -Verbose).FullName
-$uiProjectPath = Join-Path $currentPath ".\Todo.DemoPlugin.AngularUI"
-$wwwrootPath = Join-Path $uiProjectPath ".\wwwroot\demoplugin"
 $webcoreProjectPath = Join-Path $currentPath ".\Todo.DemoPlugin.Web.Core";
 $metadetaJson = Join-Path $webcoreProjectPath "Todo.DemoPlugin.json"
 $releasePath = Join-Path $webcoreProjectPath ".\bin\x64\Release\netcoreapp2.0"
@@ -10,13 +8,7 @@ $hostPath = Join-Path $currentPath "..\src\Todo.MainProject.Web.Host"
 $pluginPath = Join-Path $hostPath "PlugIns"
 $pluginDllPath = Join-Path $pluginPath "Todo.DemoPlugin"
 
-
-Set-Location $uiProjectPath
-yarn
-ng build -bh /demoplugin/ -prod -aot
-
 Set-Location $webcoreProjectPath
-Compress-Archive -Path $wwwrootPath -DestinationPath demoplugin.zip -Force
 dotnet msbuild /t:clean
 dotnet msbuild /t:rebuild /p:Configuration=Release /p:Platform=x64
 
